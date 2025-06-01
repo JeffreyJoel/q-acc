2a2d812444ea9285b6bbf4dc86cb0f3af12796f0
JeffreyJoel <jowoloko@gmail.com>
2025-06-01 20:07:54 +0100

    add verification

---
+++ src/components/verification/GitcoinLow.tsx
@@ -0,0 +1,78 @@
+import { FC } from 'react';
+import { Button } from '../ui/button';
+import links from '@/lib/constants/links';
+import { IconExternalLink } from '@tabler/icons-react';
+import { IconGitcoinPassport } from '@/components/icons/IconGitcoin';
+import { useUpdateSkipVerification } from '@/hooks/useUpdateSkipVerification';
+import { useFetchUser } from '@/hooks/useFetchUser';
+import { Address } from 'viem';
+import { useAccount } from 'wagmi';
+
+interface IGitcoinLowProps {
+  userGitcoinScore: number;
+  isScoreFetching: boolean;
+  onCheckScore: () => void;
+  onClose?: () => void;
+}
+
+export const GitcoinLow: FC<IGitcoinLowProps> = ({
+  userGitcoinScore,
+  isScoreFetching,
+  onCheckScore,
+  onClose,
+}) => {
+  const { mutate: updateSkipVerification, isPending } =
+    useUpdateSkipVerification(() => {
+      console.log('Skip verification updated successfully!');
+      if (onClose) {
+        onClose();
+      }
+    });
+
+  const { address } = useAccount();
+  const { data: user } = useFetchUser(true, address as Address);
+
+  return (
+    <div className='bg-neutral-800 my-2 rounded-xl p-4 text-base border-[1px] border-neutral-700'>
+      <div className='bg-neutral-800 mt-2 rounded-xl p-4 text-base text-white flex items-center justify-between'>
+        Your Passport Score
+        <div className='bg-black text-white py-2 px-6 rounded-full'>
+          {userGitcoinScore}
+        </div>
+      </div>
+      <div className='flex gap-2 items-center justify-end mt-4'>
+        <Button
+          className='mr-auto px-16 shadow-baseShadow'
+          variant='default'
+          disabled={isPending || user?.skipVerification}
+          onClick={() => updateSkipVerification(true)}
+        >
+          {user?.skipVerification
+            ? 'Verification Skipped '
+            : 'Skip Verification'}
+        </Button>
+
+        <a href={links.PASSPORT} target='_blank' referrerPolicy='no-referrer'>
+          <Button
+            className=' bg-pink-400'
+          >
+            <div className='flex items-center gap-1'>
+              Increase Score to 15
+              <IconExternalLink size={16} />
+            </div>
+          </Button>
+        </a>
+        <Button
+          className=' bg-pink-400'
+          onClick={onCheckScore}
+          disabled={isScoreFetching}
+        >
+          <div className='flex gap-2'>
+            <IconGitcoinPassport size={16} />
+            Refresh Score
+          </div>
+        </Button>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/verification/SkipVerification.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useRouter } from 'next/navigation';
+import { useFetchUser } from '@/hooks/useFetchUser';
+import { useUpdateSkipVerification } from '@/hooks/useUpdateSkipVerification';
+import { Button } from '../ui/button';
+import { EligibilityBadge, EligibilityBadgeStatus } from '../verification-badges/EligibilityBadge';
+import { usePrivado } from '@/hooks/usePrivado';
+import {
+  GitcoinVerificationStatus,
+  useGitcoinScore,
+} from '@/hooks/useGitcoinScore';
+import { useAccount } from 'wagmi';
+import { Address } from 'viem';
+
+const SkipVerification = () => {
+  const { address } = useAccount();
+  const { data: user } = useFetchUser(true, address as Address);
+  const { status } = useGitcoinScore(address as Address);
+  const router = useRouter();
+  const { isVerified } = usePrivado(address as Address);
+  const { mutate: updateSkipVerification, isPending } =
+    useUpdateSkipVerification(() => {
+      console.log('Skip verification updated successfully!');
+      router.push('/projects');
+    });
+  return isVerified ||
+    status === GitcoinVerificationStatus.ANALYSIS_PASS ||
+    status === GitcoinVerificationStatus.SCORER_PASS ? (
+    <section className='relative overflow-hidden bg-neutral-800 rounded-2xl p-6'>
+      <div>
+        <h1 className='text-lg font-bold'>Skip Verification</h1>
+        <p>
+          {user?.skipVerification
+            ? '  You have skipped the verification, verify above to influence the matching distribution'
+            : "Skipping Verification will allow you to spend up to approximately $1,000, but your token purchases won't influence the distribution of the matching pool."}
+        </p>
+      </div>
+      <div className='absolute top-0 left-0 right-0 bottom-0 z-10 bg-neutral-800 opacity-60'></div>
+    </section>
+  ) : user?.skipVerification ? (
+    <section className='bg-neutral-800 rounded-2xl p-6 flex gap-4 justify-between'>
+      <div>
+        <h1 className='text-lg font-bold'>Skip Verification</h1>
+        <p>
+          You have skipped the verification, verify above to influence the
+          matching distribution.
+        </p>
+      </div>
+      <div>
+        <EligibilityBadge status={EligibilityBadgeStatus.ELIGIBLE} />
+      </div>
+    </section>
+  ) : (
+    <section className='bg-neutral-800 rounded-2xl p-6 flex flex-col gap-4'>
+      <h1 className='text-lg font-bold'>Skip Verification</h1>
+      <p>
+        Allows you to spend up to approximately $1,000 but <b>not influence</b>{' '}
+        the matching pool allocation.
+      </p>
+
+      <Button
+        variant='default'
+        className='mr-auto px-16 shadow-baseShadow'
+        disabled={isPending || user?.skipVerification}
+        onClick={() => updateSkipVerification(true)}
+      >
+        {user?.skipVerification ? 'Verification Skipped ' : 'Skip Verification'}
+      </Button>
+    </section>
+  );
+};
+
+export default SkipVerification;
